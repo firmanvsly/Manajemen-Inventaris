08f60ff78062740860cdb132c81223e104df65c6
firmanvsly <firmanvsly@gmail.com>
2022-01-31 10:09:23 +0700

    add middleware

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -7,6 +7,11 @@ use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+    
     public function index()
     {
         return view('pages.auth.login');
